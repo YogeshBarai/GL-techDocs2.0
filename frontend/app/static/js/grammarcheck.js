0f3da13f6dd78a704d09003ad8d23cdccfaf4221
YogeshBarai <70328058+YogeshBarai@users.noreply.github.com>
2023-06-02 15:19:46 +0530

    Grammar Check initial frontend update

--- frontend/app/static/js/grammarcheck.js
+++ frontend/app/static/js/grammarcheck.js
@@ -1,39 +1,100 @@
-
 $(document).ready(function () {
-  console.log("ready")
+  console.log("ready");
+
+  // Event listener for the grammar check button click
   $('#grammarCheckClick').on('click', function (event) {
     try {
-      var historyInpData = { "Email": localStorage.getItem('email'), "PageNumber": 0 };
-      console.log("Getting history for :", historyInpData)
+      var inputLatexCode = document.getElementById('editor-text').value
+      var latexInputData = { "latex_code": inputLatexCode };
+      console.log("Sending LaTeX data for Grammar check:", latexInputData);
+
       $.ajax({
-        data: JSON.stringify(historyInpData),
+        data: JSON.stringify(latexInputData),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         type: 'POST',
-        url: getApiUrl('historyGet'),
+        url: getApiUrl('check-grammar'),
         success: function (data) {
-          //In case of success the data contains the JSON
-          console.log("History data !!", data)
-          // $('#error-list').html('Loading ....');
-          //let jsonHistory = {"items": [{"datetime":"60 mins ago", "desc": "John edited an item.", "operation":"Filename.ext"},{"datetime":"20 mins ago", "desc": "Johnny edited an item.", "operation":"SomeFilename.ext"},{"datetime":"10 mins ago", "desc": "Mary edited an item.", "operation":"OldFilename.ext"}]}
-          let history = '';
-          $.each(data.items, function (key, value) {
-            history += '<div class="card"><div class="card-body"><table class="table table-hover"><thead><tr><th>' + value.time + '</th></tr></thead><tbody>';
-            history += '<tr><td>' + value.action + ' file <b>' + value.doc_name + '</b></td></tr></tbody></table></div></div><br/>';
-          });
+          console.log("Grammar Check Success!!", data);
 
-          // $('#error-list').html(history);
+          // Clear the error list initially
+          $('#error-list').html('');
+
+          // Build the HTML for each grammar check result
+          data.forEach(function (result) {
+            var errorItem = $('<div class="error-item"></div>');
+            var errorDescription = $('<div class="error-description"></div>');
+            var errorWord = $('<p></p>').text(result.result['incorrect-word']);
+            var replacements = result.result.replacements;
+
+            errorDescription.append(errorWord);
+
+            if (replacements.length > 0) {
+              var replacementsList = $('<ul></ul>');
+
+              replacements.forEach(function (replacement) {
+                var replacementItem = $('<li></li>');
+                var replacementLink = $('<a href="#" class="replacement-link"></a>').text(replacement.value);
+
+                replacementLink.on('click', function (e) {
+                  e.preventDefault();
+                  // Replace the incorrect word with the selected replacement
+                  var newText = result.originalText.replace(result.result['incorrect-word'], replacement.value);
+                  // Update the editor or perform any desired action with the modified text
+                  document.getElementById('editor-text').value = newText;
+                });
+
+                replacementItem.append(replacementLink);
+                replacementsList.append(replacementItem);
+              });
+
+              errorDescription.append(replacementsList);
+            }
+
+            var changeButton = $('<button class="btn btn-outline-secondary btn-sm">Change</button>');
+            changeButton.on('click', function () {
+              // Replace the incorrect word with the first replacement
+              var newText = inputLatexCode.replace(result.result['incorrect-word'], replacements[0].value);
+              // Update the editor or perform any desired action with the modified text
+              document.getElementById('editor-text').value = newText;
+            });
+
+            errorItem.append(errorDescription);
+            errorItem.append(changeButton);
+            $('#error-list').append(errorItem);
+
+            // Event listener for mouseenter event to highlight the error word
+            errorDescription.on('mouseenter', function () {
+              highlightWord(result.result['incorrect-word']);
+            });
+
+            // Event listener for mouseleave event to clear the highlight
+            errorDescription.on('mouseleave', function () {
+              clearHighlights();
+            });
+          });
         },
         error: function (data) {
-          // in case of error we need to read response from data.responseJSON
-
-          console.log("History error data !!", data)
-          // $('#error-list').html(data);
-
+          console.log("Grammar Check Failed!!", data);
+          // Handle the error case
         }
       });
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   });
 });
+
+// Function to highlight a specific word in the editor
+function highlightWord(word) {
+  var editorText = document.getElementById('editor-text');
+  var editorContent = editorText.value;
+  var highlightedContent = editorContent.replace(new RegExp(word, 'g'), '<span class="highlight">' + word + '</span>');
+  editorText.innerHTML = highlightedContent;
+}
+
+// Function to clear the highlights in the editor
+function clearHighlights() {
+  var editorText = document.getElementById('editor-text');
+  editorText.innerHTML = editorText.value;
+}
