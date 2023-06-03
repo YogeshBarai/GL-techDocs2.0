77ba5bf81750d92521dd051d88b5f43813ee4f85
YogeshBarai <70328058+YogeshBarai@users.noreply.github.com>
2023-06-03 10:41:15 +0530

    handle no grammar error found

--- frontend/app/static/js/grammarcheck.js
+++ frontend/app/static/js/grammarcheck.js
@@ -16,63 +16,70 @@ $(document).ready(function () {
         url: getApiUrl('check-grammar'),
         success: function (data) {
           console.log("Grammar Check Success!!", data);
+          var errorItem = $('<div class="error-item"></div>');
+          var errorDescription = $('<div class="error-description"></div>');
+          var noErrorsFound = $('<p></p>').text('No Errors Found!');
 
           // Clear the error list initially
-          $('#error-list').html('');
-
-          // Build the HTML for each grammar check result
-          data.forEach(function (result) {
-            var errorItem = $('<div class="error-item"></div>');
-            var errorDescription = $('<div class="error-description"></div>');
-            var errorWord = $('<p></p>').text(result.result['incorrect-word']);
-            var replacements = result.result.replacements;
-
-            errorDescription.append(errorWord);
-
-            if (replacements.length > 0) {
-              var replacementsList = $('<ul></ul>');
-
-              replacements.forEach(function (replacement) {
-                var replacementItem = $('<li></li>');
-                var replacementLink = $('<a href="#" class="replacement-link"></a>').text(replacement.value);
-
-                replacementLink.on('click', function (e) {
-                  e.preventDefault();
-                  // Replace the incorrect word with the selected replacement
-                  var newText = result.originalText.replace(result.result['incorrect-word'], replacement.value);
-                  // Update the editor or perform any desired action with the modified text
-                  document.getElementById('editor-text').value = newText;
+          $('#error-description').html('');
+          if (data.length > 0){
+            // Build the HTML for each grammar check result
+            data.forEach(function (result) {
+              var errorWord = $('<p></p>').text(result.result['incorrect-word']);
+              var replacements = result.result.replacements;
+  
+              errorDescription.append(errorWord);
+  
+              if (replacements.length > 0) {
+                var replacementsList = $('<ul></ul>');
+  
+                replacements.forEach(function (replacement) {
+                  var replacementItem = $('<li></li>');
+                  var replacementLink = $('<a href="#" class="replacement-link"></a>').text(replacement.value);
+  
+                  replacementLink.on('click', function (e) {
+                    e.preventDefault();
+                    // Replace the incorrect word with the selected replacement
+                    var newText = result.originalText.replace(result.result['incorrect-word'], replacement.value);
+                    // Update the editor or perform any desired action with the modified text
+                    document.getElementById('editor-text').value = newText;
+                  });
+  
+                  replacementItem.append(replacementLink);
+                  replacementsList.append(replacementItem);
                 });
-
-                replacementItem.append(replacementLink);
-                replacementsList.append(replacementItem);
+  
+                errorDescription.append(replacementsList);
+              }
+  
+              var changeButton = $('<button class="btn btn-outline-secondary btn-sm">Change</button>');
+              changeButton.on('click', function () {
+                // Replace the incorrect word with the first replacement
+                var newText = inputLatexCode.replace(result.result['incorrect-word'], replacements[0].value);
+                // Update the editor or perform any desired action with the modified text
+                document.getElementById('editor-text').value = newText;
               });
-
-              errorDescription.append(replacementsList);
-            }
-
-            var changeButton = $('<button class="btn btn-outline-secondary btn-sm">Change</button>');
-            changeButton.on('click', function () {
-              // Replace the incorrect word with the first replacement
-              var newText = inputLatexCode.replace(result.result['incorrect-word'], replacements[0].value);
-              // Update the editor or perform any desired action with the modified text
-              document.getElementById('editor-text').value = newText;
+  
+              errorItem.append(errorDescription);
+              errorItem.append(changeButton);
+              $('#error-list').append(errorItem);
+  
+              // Event listener for mouseenter event to highlight the error word
+              errorDescription.on('mouseenter', function () {
+                highlightWord(result.result['incorrect-word']);
+              });
+  
+              // Event listener for mouseleave event to clear the highlight
+              errorDescription.on('mouseleave', function () {
+                clearHighlights();
+              });
             });
-
+          } else{
+            console.log('No errors found!');
+            errorDescription.append(noErrorsFound); 
             errorItem.append(errorDescription);
-            errorItem.append(changeButton);
             $('#error-list').append(errorItem);
-
-            // Event listener for mouseenter event to highlight the error word
-            errorDescription.on('mouseenter', function () {
-              highlightWord(result.result['incorrect-word']);
-            });
-
-            // Event listener for mouseleave event to clear the highlight
-            errorDescription.on('mouseleave', function () {
-              clearHighlights();
-            });
-          });
+          }
         },
         error: function (data) {
           console.log("Grammar Check Failed!!", data);
